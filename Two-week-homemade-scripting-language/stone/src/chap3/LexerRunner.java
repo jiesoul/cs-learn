277a9e41ab932e0c2162c57e9f944b4e5d1934e3
jieosul <jiesoul@gmail.com>
2017-02-24 16:16:59 +0800

    Two-week-homemade-scripting-language go on ch04

---
+++ Two-week-homemade-scripting-language/stone/src/chap3/LexerRunner.java
@@ -0,0 +1,19 @@
+package chap3;
+
+import stone.CodeDialog;
+import stone.Lexer;
+import stone.ParseException;
+import stone.Token;
+
+/**
+ * Created by soul on 17-2-24.
+ */
+public class LexerRunner {
+
+    public static void main(String[] args) throws ParseException {
+        Lexer l = new Lexer(new CodeDialog());
+        for (Token t; (t = l.read()) != Token.EOF) {
+            System.out.println("=> " + t.getText());
+        }
+    }
+}
--- Two-week-homemade-scripting-language/stone/src/stone/Stone.java
+++
@@ -1,7 +0,0 @@
-package stone;
-
-/**
- * Created by soul on 17-2-24.
- */
-public class Stone {
-}
